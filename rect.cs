e69cc5827a1a4078444debb1299555035dd3623e
TryAgain92 <43474280+TryAgain92@users.noreply.github.com>
2019-07-05 17:48:28 +0200

    Update rect.cs

--- rect.cs
+++ rect.cs
@@ -7,133 +7,11 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
-
-    public class MathForAlgebra
-    {
-        public static LinearParameterFunction getParameterFunction(Point A, Point B)
-        {
-            return new LinearParameterFunction()
-            {
-                pointOnFunction = A,
-                direction = new Point() { X = B.X - A.X, Y = B.Y - A.Y }
-            };
-        }
-
-        public static Tuple<double, double> getPointOnFunction(LinearParameterFunction g, double parameter)
-        {
-            return new Tuple<double, double>(g.pointOnFunction.X + parameter * g.direction.X, g.pointOnFunction.Y + parameter * g.direction.Y);
-        }
-
-        public static Point getCutPointFromPointToLine(LinearParameterFunction g1, LinearParameterFunction g2)
-        {
-            if (g1.direction.Y == 0)
-            {
-                double mu = (double)(g1.pointOnFunction.Y - g2.pointOnFunction.Y) / g2.direction.Y;
-                Tuple<double,double>PointAsTuple =  getPointOnFunction(g2, mu);
-                return new Point() { X = (int)PointAsTuple.Item1, Y = (int)PointAsTuple.Item2 };
-            }
-            else
-            {
-                double l = (double)(g1.pointOnFunction.X - g2.pointOnFunction.X) / g2.direction.X;
-                Tuple<double, double> PointAsTuple = getPointOnFunction(g2, l);
-                return new Point() { X = (int)PointAsTuple.Item1, Y = (int)PointAsTuple.Item2 };
-            }
-        }
-    }
-
-    public class Point
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-    }
-
-    public class LinearParameterFunction
-    {
-        public Point pointOnFunction { get; set; }
-        public Point direction { get; set; }
-    }
-
-    public class Rect
-    {
-        public Point downLeft { get; set; }
-        public Point upperLeft { get; set; }
-        public Point upperRight { get; set; }
-        public Point downRight { get; set; }
-
-        public LinearParameterFunction leftLine { get; set; }
-        public LinearParameterFunction rightLine { get; set; }
-        public LinearParameterFunction upperLine { get; set; }
-        public LinearParameterFunction downLine { get; set; }
-
-        public bool isPointInRect(Point P)
-        {
-            Point cutPLeftSide = MathForAlgebra.getCutPointFromPointToLine(
-                new LinearParameterFunction()
-                {
-                    pointOnFunction = P,
-                    direction = new Point() { X = -1, Y = 0 }
-                },
-                leftLine);
-
-            Point cutPRightSide = MathForAlgebra.getCutPointFromPointToLine(
-                new LinearParameterFunction()
-                {
-                    pointOnFunction = P,
-                    direction = new Point() { X = 1, Y = 0 }
-                },
-                rightLine);
-
-            Point cutPUpperSide = MathForAlgebra.getCutPointFromPointToLine(
-                new LinearParameterFunction()
-                {
-                    pointOnFunction = P,
-                    direction = new Point() { X = 0, Y = 1 }
-                },
-                upperLine);
-
-            Point cutPDownSide = MathForAlgebra.getCutPointFromPointToLine(
-                new LinearParameterFunction()
-                {
-                    pointOnFunction = P,
-                    direction = new Point() { X = 0, Y = -1 }
-                },
-                downLine);
-
-            if (P.X >= cutPLeftSide.X && P.X <= cutPRightSide.X && P.Y <= cutPUpperSide.Y && P.Y >= cutPDownSide.Y)
-                return true;
-
-            return false;
-        }
-        public List<Point> getAllPointsInRect()
-        {
-            Point maxLeftPoint = downLeft.X < upperLeft.X ? downLeft : upperLeft;
-            Point maxTopPoint = upperLeft.Y > upperRight.Y ? upperLeft : upperRight;
-            Point maxRightPoint = upperRight.X > downRight.X ? upperRight : downRight;
-            Point maxBottomPoint = downRight.Y < downLeft.Y ? downRight : downLeft;
-
-            List<Point> pointsInRect = new List<Point>();
-
-            for (int x_coordinate = maxLeftPoint.X; x_coordinate <= maxRightPoint.X; x_coordinate++)
-            {
-                for (int y_coordinate = maxBottomPoint.Y; y_coordinate <= maxTopPoint.Y; y_coordinate++)
-                {
-                    Point currentPoint = new Point() { X = x_coordinate, Y = y_coordinate };
-                    if (isPointInRect(currentPoint))
-                    {
-                        pointsInRect.Add(currentPoint);
-                    }
-                }
-            }
-
-            return pointsInRect;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            //Zum Ausprobieren - - 
+            //QUICKSTART - - 
             List<Point> P = new List<Point>()
             {
                 new Point() { X = 1, Y = 1 },
@@ -141,24 +19,7 @@ namespace ConsoleApplication2
                 new Point() {X = 11, Y = 7 },
                 new Point() {X = 8, Y = 1 }
             };
-            Rect Test = set_Rect_Points(P);
 
-            Debug.WriteLine("DOWNLEFT: " + Test.downLeft.X + "/" + Test.downLeft.Y);
-            Debug.WriteLine("UPPERLEFT: " + Test.upperLeft.X + "/" + Test.upperLeft.Y);
-            Debug.WriteLine("DOWNRIGHT: " + Test.downRight.X + "/" + Test.downRight.Y);
-            Debug.WriteLine("UPPERRIGHT: " + Test.upperRight.X + "/" + Test.upperRight.Y);
-
-            Point inR = new Point() { X = 7, Y = 3 };
-            Point outR = new Point() { X = 1, Y = 2 };
-
-            Debug.WriteLine("LEFT: " + Test.leftLine.direction.X + "/" + Test.leftLine.direction.Y);
-            Debug.WriteLine("TOP: " + Test.upperLine.direction.X + "/" + Test.upperLine.direction.Y);
-            Debug.WriteLine("RIGHT: " + Test.rightLine.direction.X + "/" + Test.rightLine.direction.Y);
-            Debug.WriteLine("BOTTOM: " + Test.downLine.direction.X + "/" + Test.downLine.direction.Y);
-
-
-            Debug.WriteLine("P(" + inR.X + "/" + inR.Y + ") ist in Rect: " + Test.isPointInRect(inR));
-            Debug.WriteLine("P(" + outR.X + "/" + outR.Y + ") ist in Rect: " + Test.isPointInRect(outR));
 
             List<Point> pInR = Test.getAllPointsInRect();
             foreach (Point Q in pInR)
@@ -167,34 +28,100 @@ namespace ConsoleApplication2
             }
 
         }
-
-        // Please Just a List with 4 Points!!!
-        public static Rect set_Rect_Points(List<Point> Points)
+        
+         public static bool pointIsInRect(Point P, List<Point> cornerPoints)
         {
-            List<Point> RightLeftSortedPoints = Points.OrderBy(point => point.X).ToList();
+            List<Point> RightLeftSortedPoints = cornerPoints.OrderBy(point => point.X).ToList();
             List<Point> LeftPoints = RightLeftSortedPoints.GetRange(0, 2);
             List<Point> RightPoints = RightLeftSortedPoints.GetRange(2, 2);
 
             List<Point> upperDownSortedByLeftPoints = LeftPoints.OrderBy(point => point.Y).ToList();
             List<Point> upperDownSortedByRightPoints = RightPoints.OrderBy(point => point.Y).ToList();
 
-            Point downLeft = upperDownSortedByLeftPoints[0];
-            Point upperLeft = upperDownSortedByLeftPoints[1];
-            Point downRight = upperDownSortedByRightPoints[0];
-            Point upperRight = upperDownSortedByRightPoints[1];
+            Point bottomLeft = upperDownSortedByLeftPoints[0];
+            Point topLeft = upperDownSortedByLeftPoints[1];
+            Point bottomRight = upperDownSortedByRightPoints[0];
+            Point topRight = upperDownSortedByRightPoints[1];
 
-            return new Rect()
+            List<Point> leftLine = new List<Point>() { bottomLeft, new Point() { X = topLeft.X - bottomLeft.X, Y = topLeft.Y - bottomLeft.Y } };
+            List<Point> topLine = new List<Point>() { topLeft, new Point() { X = topRight.X - topLeft.X, Y = topRight.Y - topLeft.Y } };
+            List<Point> rightLine = new List<Point>() { topRight, new Point() { X = bottomRight.X - topRight.X, Y = bottomRight.Y - topRight.Y } };
+            List<Point> bottomLine = new List<Point>() { bottomRight, new Point() { X = bottomLeft.X - bottomRight.X, Y = bottomLeft.Y - bottomRight.Y } };
+
+            List<List<Point>> squareLines =  new List<List<Point>>() { leftLine, topLine, rightLine, bottomLine };
+
+            double parameterPointToLeftSide = (double)(P.Y - squareLines[0][0].Y) / squareLines[0][1].Y;
+            Tuple<double, double> shortestPointOnLeftLineToPointAsDouble = new Tuple<double, double>(
+                    squareLines[0][0].X + parameterPointToLeftSide * squareLines[0][1].X,
+                    squareLines[0][0].Y + parameterPointToLeftSide * squareLines[0][1].Y
+                );
+            Point shortestPointOnLeftLineToPoint = new Point() { X = (int)shortestPointOnLeftLineToPointAsDouble.Item1, Y = (int)shortestPointOnLeftLineToPointAsDouble.Item2 };
+
+
+            double parameterPointToTopSide = (double)(P.X - squareLines[1][0].X) / squareLines[1][1].X;
+            Tuple<double, double> shortestPointOnTopLineToPointAsDouble = new Tuple<double, double>(
+                    squareLines[1][0].X + parameterPointToTopSide * squareLines[1][1].X,
+                    squareLines[1][0].Y + parameterPointToTopSide * squareLines[1][1].Y
+                );
+            Point shortestPointOnTopLineToPoint = new Point() { X = (int)shortestPointOnTopLineToPointAsDouble.Item1, Y = (int)shortestPointOnTopLineToPointAsDouble.Item2 };
+
+
+            double parameterPointToRightSide = (double)(P.Y - squareLines[2][0].Y) / squareLines[2][1].Y;
+            Tuple<double, double> shortestPointOnRightLineToPointAsDouble = new Tuple<double, double>(
+                    squareLines[2][0].X + parameterPointToRightSide * squareLines[2][1].X,
+                    squareLines[2][0].Y + parameterPointToRightSide * squareLines[2][1].Y
+                );
+            Point shortestPointOnRightLineToPoint = new Point() { X = (int)shortestPointOnRightLineToPointAsDouble.Item1, Y = (int)shortestPointOnRightLineToPointAsDouble.Item2 };
+
+
+            double parameterPointToBottomSide = (double)(P.X - squareLines[3][0].X) / squareLines[2][1].X;
+            Tuple<double, double> shortestPointOnBottomLineToPointAsDouble = new Tuple<double, double>(
+                    squareLines[3][0].X + parameterPointToBottomSide * squareLines[3][1].X,
+                    squareLines[3][0].Y + parameterPointToBottomSide * squareLines[3][1].Y
+                );
+            Point shortestPointOnBottomLineToPoint = new Point() { X = (int)shortestPointOnBottomLineToPointAsDouble.Item1, Y = (int)shortestPointOnBottomLineToPointAsDouble.Item2 };
+
+            if (P.X >= shortestPointOnLeftLineToPoint.X && P.X <= shortestPointOnRightLineToPoint.X && P.Y <= shortestPointOnTopLineToPoint.Y && P.Y >= shortestPointOnBottomLineToPoint.Y)
+                return true;
+
+            return false;
+        }
+
+
+        public static List<Point> getPointsFromRect(List<Point> cornerPoints)
+        {
+            List<Point> RightLeftSortedPoints = cornerPoints.OrderBy(point => point.X).ToList();
+            List<Point> LeftPoints = RightLeftSortedPoints.GetRange(0, 2);
+            List<Point> RightPoints = RightLeftSortedPoints.GetRange(2, 2);
+
+            List<Point> upperDownSortedByLeftPoints = LeftPoints.OrderBy(point => point.Y).ToList();
+            List<Point> upperDownSortedByRightPoints = RightPoints.OrderBy(point => point.Y).ToList();
+
+            Point bottomLeft = upperDownSortedByLeftPoints[0];
+            Point topLeft = upperDownSortedByLeftPoints[1];
+            Point bottomRight = upperDownSortedByRightPoints[0];
+            Point topRight = upperDownSortedByRightPoints[1];
+
+            Point maxLeftPoint = bottomLeft.X < topLeft.X ? bottomLeft: topLeft;
+            Point maxTopPoint = topLeft.Y > bottomRight.Y ? topLeft: bottomRight;
+            Point maxRightPoint = topRight.X > bottomRight.X ? topRight: bottomRight;
+            Point maxBottomPoint = bottomRight.Y < bottomLeft.Y ? bottomRight : bottomLeft;
+
+            List<Point> pointsInRect = new List<Point>();
+
+            for (int x_coordinate = maxLeftPoint.X; x_coordinate <= maxRightPoint.X; x_coordinate++)
             {
-                downLeft = downLeft,
-                upperLeft = upperLeft,
-                downRight = downRight,
-                upperRight = upperRight,
+                for (int y_coordinate = maxBottomPoint.Y; y_coordinate <= maxTopPoint.Y; y_coordinate++)
+                {
+                    Point currentPoint = new Point() { X = x_coordinate, Y = y_coordinate };
+                    if (pointIsInRect(currentPoint, cornerPoints))
+                    {
+                        pointsInRect.Add(currentPoint);
+                    }
+                }
+            }
 
-                leftLine = MathForAlgebra.getParameterFunction(downLeft, upperLeft),
-                upperLine = MathForAlgebra.getParameterFunction(upperLeft, upperRight),
-                rightLine = MathForAlgebra.getParameterFunction(upperRight, downRight),
-                downLine = MathForAlgebra.getParameterFunction(downRight, downLeft)
-            };
+            return pointsInRect;
         }
 
 
